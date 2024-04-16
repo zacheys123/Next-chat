d8b27a5729f352c455111e9f2989940bdcf48b89
zacharia muigai <user.zacheyshop@gmail.com>
2024-04-16 03:26:39 -0700

    ne

--- app/not-found.js
+++ app/not-found.js
@@ -18,3 +18,72 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
+export async function POST(req) {
+  // const { search_Ref, auth0 } = await req.json();
+  // console.log(search_Ref.current);
+  // if (
+  //   !search_Ref.current ||
+  //   search_Ref.current.length < 0 ||
+  //   search_Ref.current === "undefined"
+  // ) {
+  //   return NextResponse.json({ message: "Input is required" }, { status: 403 });
+  // } else {
+  //   try {
+  //     await connectDb();
+  //     // const newsearch_Ref.current = req.query.search;
+  //     const user = await User.findOne({ auth0Id: { $en: auth0 } });
+  //     if (user) {
+  //       if (user !== search_Ref.current) {
+  //         return NextResponse.json(
+  //           { message: "user not found" },
+  //           { status: 403 }
+  //         );
+  //       }
+  //       await User.findOne({
+  //         $or: [
+  //           { firstname: search_Ref.current },
+  //           { secondName: search_Ref.current },
+  //           { email: search_Ref.current },
+  //           { instrument: search_Ref.current },
+  //           { username: search_Ref.current },
+  //         ],
+  //       })
+  //         .collation({ locale: "en", strength: 2 })
+  //         .exec();
+  //       return NextResponse.json({ user });
+  //     }
+  //     return NextResponse.json(
+  //       { message: "You cannot search ur data" },
+  //       { status: 403 }
+  //     );
+  //   } catch (error) {
+  //     return NextResponse.json({ error: error.message });
+  //   }
+  // }
+}
+
+// const user = await User.findOne({
+//   $or: [
+//     { firstname: { $regex: req.query.search, $options: "i" } },
+//     { secondname: { $regex: req.query.search, $options: "i" } },
+//     { firstname: { $regex: req.query.search, $options: "i" } },
+//     { firstname: { $regex: req.query.search, $options: "i" } },
+//   ],
+// });
+// .find({ _id: { $ne: req.query._id } });
+
+// find a user dynamically
+// export async function GET(req) {
+// const { username } = await req.json();
+// try {
+//   await connectDb();
+//   const user = await User.findOne({ username });
+//   if (!user) {
+//     return NextResponse.json({ message: "user not found" });
+//   }
+//   return NextResponse.json({ user });
+// } catch (error) {
+//   return NextResponse.json({ error: error.message });
+// }
+// }
